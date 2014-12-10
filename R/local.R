89d1cf1ff994805d35314e41a627d6b3ad6c67ae
Kenny Darrell <darrelkj@gmail.com>
2014-12-10 13:25:29 -0500

    condense code and modify arg name

--- R/local.R
+++ R/local.R
@@ -4,9 +4,9 @@
 #'
 #' Function to compute a set of measures for each vertex in a given network.
 #'
-#' @usage localStats(g, stats)
+#' @usage localStats(g, loc)
 #' @param g An igraph object
-#' @param stats A list of which netowrk parameters should be calculated
+#' @param loc A list of which netowrk parameters should be calculated
 #' @export
 #'
 #' @details Will add to the parameteres into a dataframe with the vertex name
@@ -20,15 +20,15 @@
 #' localStats(karate, c("degree", "closeness", 'hub'))
 
 
-localStats <- function(g, stats) {
+localStats <- function(g, loc) {
   as.data.frame(cbind(
-      name = vertex.attributes(g)$name,
-           degree = if('degree'      %in% stats) centralization.degree(g)$res,
-        closeness = if('closeness'   %in% stats) centralization.closeness(g)$res,
-       betweeness = if('betweeness'  %in% stats) betweenness(g),
-      eigenvector = if('eigenvector' %in% stats) centralization.evcent(g)$vector,
-              hub = if('hub'         %in% stats) hub.score(g)$vector,
-             auth = if('auth'        %in% stats) authority.score(g)$vector,
-             page = if('page'        %in% stats) page.rank(g)$vector), 
+             name = vertex.attributes(g)$name,
+           degree = if('degree'      %in% loc) centralization.degree(g)$res,
+        closeness = if('closeness'   %in% loc) centralization.closeness(g)$res,
+       betweeness = if('betweeness'  %in% loc) betweenness(g),
+      eigenvector = if('eigenvector' %in% loc) centralization.evcent(g)$vector,
+              hub = if('hub'         %in% loc) hub.score(g)$vector,
+             auth = if('auth'        %in% loc) authority.score(g)$vector,
+             page = if('page'        %in% loc) page.rank(g)$vector), 
     row.names = 1:length(V(g)))
 }
