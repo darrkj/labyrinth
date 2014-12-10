09816968b91e666a958f54ce45396999a2c6cd74
Kenny Darrell <darrelkj@gmail.com>
2014-12-10 06:20:15 -0500

    pull stats out

--- R/mazewalker.R
+++ R/mazewalker.R
@@ -1,37 +1,4 @@
 
-localStats <- function(g, stats) {
-  as.data.frame(cbind(name = vertex.attributes(g)$name,
-                      degree = if('degree' %in% stats) centralization.degree(g)$res else NULL,
-                      closeness = if('closeness' %in% stats) centralization.closeness(g)$res else NULL,
-                      betweeness = if('betweeness' %in% stats) betweenness(g) else NULL,
-                      eigenvector = if('eigenvector' %in% stats) centralization.evcent(g)$vector else NULL,
-                      # alpha.centrality(g), bonpow = bonpow(g)
-                      hub = if('hub' %in% stats) hub.score(g)$vector else NULL,
-                      auth = if('auth' %in% stats) authority.score(g)$vector else NULL,
-                      page = if('page' %in% stats) page.rank(g)$vector else NULL), row.names = 1:length(V(g)))
-}
-
-
-globalStats <- function(g) { 
-  data.frame(
-    Degree = length(V(g)),
-    Degree_Centralization = centralization.degree(g)$centralization,
-    Closeness_Centralization = centralization.closeness(g)$centralization,
-    Betweenness_Centralization = centralization.betweenness(g, directed = FALSE)$centralization,
-    Eigenvector_Centralization = centralization.evcent(g, directed = FALSE)$centralization,
-    Assortativity_Coefficient = assortativity.degree(g),
-    Average_Path_Length = average.path.length(g, directed = FALSE, unconnected = TRUE),
-    Clique_Number = clique.number(g),
-    Diameter = diameter(g),
-    Radius = radius(g),
-    Girth = girth(g)$girth,
-    Adhesion = graph.adhesion(g),
-    Density = graph.density(g),
-    Chordal = is.chordal(g)$chordal,
-    Connected = is.connected(g)
-  )
-}
-
 
 # This is all that is needed for adding measures.
 
