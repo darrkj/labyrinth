12baf7b73cb12fa166f2e05f68acbff3ae49c480
Kenny Darrell <darrelkj@gmail.com>
2014-12-10 06:41:30 -0500

    add more roxy comments

--- R/clearDomain.R
+++ R/clearDomain.R
@@ -1,9 +1,19 @@
 
+#' Clear Neo4j sub-graph
+#'
+#' Clear sub-graph in Neo4j with given domain.
+#'
+#' @usage clearDomain(domain)
+#' @param domain A string of which is attached to nodes and relationships as the domain property in the database
+#' 
+#' @export
+#'
+#' @examples
+#' clearDomain('karate')
+
+
 clearDomain <- function(domain) {
   cypher(graph, paste0("match (a)-[r]->(b) where r.domain = '", domain, "' delete r;"))
   cypher(graph, paste0("match a where a.domain = '", domain, "' delete a;"))
 }
 
-
-
-# clearDom('karate')
--- R/local.R
+++ R/local.R
@@ -1,3 +1,25 @@
+
+
+#' Vertex level measures
+#'
+#' Function to compute a set of measures for each vertex in a given network.
+#'
+#' @usage localStats(g, stats)
+#' @param g An igraph object
+#' @param stats A list of which netowrk parameters should be calculated
+#' @export
+#'
+#' @details Will add to the parameteres into a dataframe with the vertex name
+#'
+#' @examples
+#' require(igraph)
+#' require(igraphdata)
+#'   
+#' data(karate)
+#' 
+#' localStats(karate, c("degree", "closeness", 'hub'))
+
+
 localStats <- function(g, stats) {
   as.data.frame(cbind(name = vertex.attributes(g)$name,
                       degree = if('degree' %in% stats) centralization.degree(g)$res else NULL,
