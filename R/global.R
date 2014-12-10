848f712ef1287fcf1f62739e80af013c81bc5929
Kenny Darrell <darrelkj@gmail.com>
2014-12-10 08:12:03 -0500

    fix global stats to work more like local

--- R/global.R
+++ R/global.R
@@ -2,22 +2,23 @@
 
 
 
-globalStats <- function(g) { 
-  data.frame(
-    Degree = length(V(g)),
-    Degree_Centralization = centralization.degree(g)$centralization,
-    Closeness_Centralization = centralization.closeness(g)$centralization,
-    Betweenness_Centralization = centralization.betweenness(g, directed = FALSE)$centralization,
-    Eigenvector_Centralization = centralization.evcent(g, directed = FALSE)$centralization,
-    Assortativity_Coefficient = assortativity.degree(g),
-    Average_Path_Length = average.path.length(g, directed = FALSE, unconnected = TRUE),
-    Clique_Number = clique.number(g),
-    Diameter = diameter(g),
-    Radius = radius(g),
-    Girth = girth(g)$girth,
-    Adhesion = graph.adhesion(g),
-    Density = graph.density(g),
-    Chordal = is.chordal(g)$chordal,
-    Connected = is.connected(g)
-  )
+globalStats <- function(g, glob, dir = FALSE) { 
+  as.data.frame(cbind(
+    name = vertex.attributes(g)$name,
+    Degree = if('Degree' %in% glob) length(V(g)) else NULL,
+    Degree_Cent = if('Degree_Cent' %in% glob) centralization.degree(g)$centralization, else NULL,
+    Closeness_Cent = if('Closeness_Cent' %in% glob) centralization.closeness(g)$centralization else NULL,
+    Betweenness_Cent = if('Betweenness_Cent' %in% glob) centralization.betweenness(g, directed = dir)$centralization else NULL,
+    Eigenvector_Cent = if('Eigenvector_Cent' %in% glob) centralization.evcent(g, directed = dir)$centralization else NULL,
+    Assortativity = if('Assortativity' %in% glob) assortativity.degree(g) else NULL,
+    Avg_Path_Length = if('Avg_Path_Length' %in% glob) average.path.length(g, directed = dir, unconnected = TRUE) else NULL,
+    Clique_Number = if('Clique_Number' %in% glob) clique.number(g) else NULL,
+    Diameter = if('Diameter' %in% glob) diameter(g) else NULL,
+    Radius = if('Radius' %in% glob) radius(g) else NULL,
+    Girth = if('Girth' %in% glob) girth(g)$girth else NULL,
+    Adhesion = if('Adhesion' %in% glob) graph.adhesion(g) else NULL,
+    Density = if('Density' %in% glob) graph.density(g) else NULL,
+    Chordal = if('Chordal' %in% glob) is.chordal(g)$chordal else NULL,
+    Connected = if('Connected' %in% glob) is.connected(g) else NULL),
+    row.names = 1:length(V(g)))
 }
