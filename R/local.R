21b1a7b1766dd786a2ca7adbd9217c880ce72ef7
Kenny Darrell <darrelkj@gmail.com>
2014-12-10 12:59:12 -0500

    remove else NULL that happen any way

--- R/local.R
+++ R/local.R
@@ -23,13 +23,13 @@
 localStats <- function(g, stats) {
   as.data.frame(cbind(
       name = vertex.attributes(g)$name,
-      degree = if('degree' %in% stats) centralization.degree(g)$res else NULL,
-      closeness = if('closeness' %in% stats) centralization.closeness(g)$res else NULL,
-      betweeness = if('betweeness' %in% stats) betweenness(g) else NULL,
-      eigenvector = if('eigenvector' %in% stats) centralization.evcent(g)$vector else NULL,
+      degree = if('degree' %in% stats) centralization.degree(g)$res,
+      closeness = if('closeness' %in% stats) centralization.closeness(g)$res,
+      betweeness = if('betweeness' %in% stats) betweenness(g),
+      eigenvector = if('eigenvector' %in% stats) centralization.evcent(g)$vector,
             # alpha.centrality(g), bonpow = bonpow(g)
-      hub = if('hub' %in% stats) hub.score(g)$vector else NULL,
-      auth = if('auth' %in% stats) authority.score(g)$vector else NULL,
-      page = if('page' %in% stats) page.rank(g)$vector else NULL), 
+      hub = if('hub' %in% stats) hub.score(g)$vector,
+      auth = if('auth' %in% stats) authority.score(g)$vector,
+      page = if('page' %in% stats) page.rank(g)$vector), 
     row.names = 1:length(V(g)))
 }
