5e338701a4edc1d687c6be709dfe35019b823f6f
Kenny Darrell <darrelkj@gmail.com>
2014-12-10 13:15:27 -0500

    cleanup local function

--- R/local.R
+++ R/local.R
@@ -23,13 +23,12 @@
 localStats <- function(g, stats) {
   as.data.frame(cbind(
       name = vertex.attributes(g)$name,
-      degree = if('degree' %in% stats) centralization.degree(g)$res,
-      closeness = if('closeness' %in% stats) centralization.closeness(g)$res,
-      betweeness = if('betweeness' %in% stats) betweenness(g),
+           degree = if('degree'      %in% stats) centralization.degree(g)$res,
+        closeness = if('closeness'   %in% stats) centralization.closeness(g)$res,
+       betweeness = if('betweeness'  %in% stats) betweenness(g),
       eigenvector = if('eigenvector' %in% stats) centralization.evcent(g)$vector,
-            # alpha.centrality(g), bonpow = bonpow(g)
-      hub = if('hub' %in% stats) hub.score(g)$vector,
-      auth = if('auth' %in% stats) authority.score(g)$vector,
-      page = if('page' %in% stats) page.rank(g)$vector), 
+              hub = if('hub'         %in% stats) hub.score(g)$vector,
+             auth = if('auth'        %in% stats) authority.score(g)$vector,
+             page = if('page'        %in% stats) page.rank(g)$vector), 
     row.names = 1:length(V(g)))
 }
