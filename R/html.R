bf8a0e88ae4c7ceb3d4b33404a34672879bb892a
Kenny Darrell <darrelkj@gmail.com>
2014-12-14 15:32:07 -0500

    add roxy comments

--- R/html.R
+++ R/html.R
@@ -1,3 +1,17 @@
+
+
+#' html
+#'
+#' Plot html to viewer
+#'
+#' @usage html(file)
+#' @export
+#' @param file An html file
+#'
+#'
+#' @examples
+#' neoSankey('file.html')
+
 html <- function(file) {
   # Create temporary html file
   htmlFile <- tempfile(fileext=".html")
