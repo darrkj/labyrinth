086be34d2a7bf463c4a66578b9fb396289dcc617
Kenny Darrell <darrelkj@gmail.com>
2014-12-14 15:28:55 -0500

    get sankey function working

--- R/sankey.R
+++ R/sankey.R
@@ -1,65 +1,60 @@
-plotRel <- function() {
-
-fight <- cypher(graph, 'MATCH (a)-[r]->(b) where a.domain = "senate"
-                RETURN head(labels(a)) AS This, type(r) as To, head(labels(b)) AS That')
 
 
-d1 <- data.frame(a = unique(fight$This))
-d2 <- data.frame(a = unique(fight$To))
-d3 <- data.frame(a = unique(fight$That))
-
-d1$n <- 0:(nrow(d1)-1)
-d2$n <- (max(d1$n)+1):(max(d1$n) + nrow(d2))
-d3$n <- (max(d2$n)+1):(max(d2$n) + nrow(d3))
-
-
-
-nod <- rbind(d1, d2, d3)
-node <- nod
-
-nod$a <- NULL
-names(nod) <- 'name'
-
-node$n <- NULL
-names(node) <- 'name'
-
-#class or country to any to class or country
-# a to b
-
-fight %>% 
-  select(sourcen = This, targetn = To) %>%
-  inner_join(d1, by = c('sourcen' = 'a')) %>%
-  select(source = n, targetn) %>%
-  inner_join(d2, by = c('targetn' = 'a')) %>%
-  select(source, target = n) %>%
-  group_by(source, target) %>%
-  mutate(value = n()) %>% 
-  distinct -> ab
+#' neoSankey
+#'
+#' Sankey plot of relationships in database
+#'
+#' @usage neoSankey()
+#' @export
+#'
+#'
+#' @examples
+#' neoSankey()
 
 
+neoSankey <- function(constraints = NULL) {
+  
+  if (!is.null(constraints)) {
+    constraints <- paste0("WHERE a.domain = '", constraints, "'")
+  }
+  
+  rel <- cypher(graph, paste(
+    'MATCH (a)-[r]->(b)', constraints, 
+    'RETURN head(labels(a)) AS This, type(r) as To, head(labels(b)) AS That'))
 
-fight %>%
-  select(sourcen = To, targetn = That) %>%
-  inner_join(d2, by = c('sourcen' = 'a')) %>%
-  select(source = n, targetn) %>%
-  inner_join(d3, by = c('targetn' = 'a')) %>%
-  select(source, target = n) %>%
-  group_by(source, target) %>%
-  mutate(value = n()) %>% 
-  distinct -> bc
+  nd <- as.data.frame(rbind(cbind(unique(rel$This), 'a'),
+                            cbind(unique(rel$To), 'b'),
+                            cbind(unique(rel$That), 'c')))
+  
+  nd <- nd[complete.cases(nd), ]
+  nd$n <- 0:(nrow(nd)-1)
 
+  rel %>% 
+    select(sourcen = This, targetn = To) %>%
+    inner_join(nd[nd$V2 == 'a', ], by = c('sourcen' = 'V1')) %>%
+    select(source = n, targetn) %>%
+    inner_join(nd[nd$V2 == 'b', ], by = c('targetn' = 'V1')) %>%
+    select(source, target = n) %>%
+    group_by(source, target) %>%
+    mutate(value = n()) %>% distinct -> ab
 
+  rel %>%
+    select(sourcen = To, targetn = That) %>%
+    inner_join(nd[nd$V2 == 'b', ], by = c('sourcen' = 'V1')) %>%
+    select(source = n, targetn) %>%
+    inner_join(nd[nd$V2 == 'c', ], by = c('targetn' = 'V1')) %>%
+    select(source, target = n) %>%
+    group_by(source, target) %>%
+    mutate(value = n()) %>% distinct -> bc
+  
+  link <- as.data.frame(rbind(ab, bc))
 
+  d3Sankey(Links = link, Nodes = nd, Source = "source", width = 600,
+           Target = "target", Value = "value", NodeID = "V1",
+           fontsize = 12, nodeWidth = 30, file = 'tmp.html')
 
-link <- as.data.frame(rbind(ab, bc))
+  suppressWarnings(html('tmp.html'))
 
-
-
-d3Sankey(Links = link, Nodes = node, Source = "source",
-         Target = "target", Value = "value", NodeID = "name",
-         fontsize = 12, nodeWidth = 30, file = 'tmp.html')
-
-suppressWarnings('tmp.html')
-
-
+  return(invisible(NULL))
+}
 
